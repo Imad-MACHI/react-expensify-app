eb7b301f05ddd34c61f7eb0ab1a2bfc036c977d7
Imad MACHI <imadoxmachi@gmail.com>
2020-10-23 11:40:12 +0000

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require("path");
-
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = (env) => {
   const isProduction = env === "production";
   const CSSExtract = new ExtractTextPlugin("styles.css");
-  console.log("env", env);
+
   return {
     entry: "./src/app.js",
     output: {
@@ -23,8 +22,18 @@ module.exports = (env) => {
           test: /\.s?css$/,
           use: CSSExtract.extract({
             use: [
-              { loader: "css-loader", options: { sourceMap: true } },
-              { loader: "sass-loader", options: { sourceMap: true } },
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  sourceMap: true,
+                },
+              },
             ],
           }),
         },
